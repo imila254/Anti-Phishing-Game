df88dcd6a232b4de26153cec9eb9e40c21eb2445
GMurnevaite <greta.murnevaite@gmail.com>
2023-04-29 18:31:58 +0300

    Drawer opening added

--- 2D Escape Room/Assets/Scripts/ChangeView.cs
+++ 2D Escape Room/Assets/Scripts/ChangeView.cs
@@ -7,17 +7,25 @@ public class ChangeView : MonoBehaviour, IInteractable
 
     public string SpriteName;
 
+    private float initialCameraSize;
+
+    private Vector3 initialCameraPosition;
+
     public void Interact(DisplayImage currentDisplay)
     {
         currentDisplay.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/" + SpriteName);
 
         currentDisplay.CurrentState = DisplayImage.State.ChangedView;
+
+        Camera.main.orthographicSize = initialCameraSize;
+        Camera.main.transform.position = initialCameraPosition;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialCameraSize = Camera.main.orthographicSize;
+        initialCameraPosition = Camera.main.transform.position;
     }
 
     // Update is called once per frame
