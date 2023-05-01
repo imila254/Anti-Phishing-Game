a4c4c93b8a7cd9a25e7cc13050627febeb75b90d
GMurnevaite <greta.murnevaite@gmail.com>
2023-05-02 02:08:11 +0300

    Added new objects

--- 2D Escape Room/Assets/Scripts/UIDisplayer.cs
+++ 2D Escape Room/Assets/Scripts/UIDisplayer.cs
@@ -11,10 +11,12 @@ public class UIDisplayer : MonoBehaviour, IInteractable
     {
         DisplayObject.SetActive(true);
     }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
