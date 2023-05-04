00c80e4612d9d8d89370d525a4ad6317afca076f
imila254 <milasevaiteingrida@gmail.com>
2023-05-04 10:06:28 +0300

    Intro Scene added

--- 2D Escape Room/Assets/Scripts/SceneController/ChangeOnClick.cs
+++ 2D Escape Room/Assets/Scripts/SceneController/ChangeOnClick.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ChangeOnClick : MonoBehaviour
 {
@@ -13,5 +14,16 @@ public class ChangeOnClick : MonoBehaviour
         {
             SceneManager.LoadScene(SceneToBeLoaded);
         }
+    }
+
+    public void ChangeViewOnButtonClicked()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(ButtonClicked);
+    }
+
+    void ButtonClicked()
+    {
+        Debug.Log("Clicked");
+        SceneManager.LoadScene(SceneToBeLoaded);
     }
 }
