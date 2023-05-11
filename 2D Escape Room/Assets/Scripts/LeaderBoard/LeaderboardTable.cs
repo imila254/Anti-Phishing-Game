bff74b04630a1919f25f329b22d812621c655ff1
imila254 <milasevaiteingrida@gmail.com>
2023-05-11 21:59:05 +0300

    scenes updated

--- 2D Escape Room/Assets/Scripts/LeaderBoard/LeaderboardTable.cs
+++ 2D Escape Room/Assets/Scripts/LeaderBoard/LeaderboardTable.cs
@@ -25,11 +25,6 @@ public class LeaderboardTable : MonoBehaviour
         Highscores highscores = JsonUtility.FromJson<Highscores>(jsonStringRes);
 
 
-        for (int i = 0; i < highscores.HighscoresEntries.Count; i++)
-        {
-            Debug.Log(highscores.HighscoresEntries[i].name + "  " + highscores.HighscoresEntries[i].time1 + "  " + highscores.HighscoresEntries[i].time2 + "  " + highscores.HighscoresEntries[i].time3);
-        }
-
 
         for (int i = 0; i < highscores.HighscoresEntries.Count; i++)
         {
